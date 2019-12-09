2b92cab67800491c6f7e67600f7a6d2a6e0ac816
David Ogle <dogle@scottlogic.com>
2019-12-09 13:55:25 +0000

    add functional css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,40 @@
-import ChildComponent from 'components/child';
-import React from 'react';
-import logo from './logo.svg';
+// import ChildComponent from "components/child";
+import ChildComponent from "components/funcChild";
+import React from "react";
+import classnames from "classnames";
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
 // Test css
 // import './pure.css';
 
 // Test scss
-import './app.scss';
+import "./app.scss";
+
+// Test functional css
+import "./functional.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <span><img src={logo} className="App-logo" alt="logo" />
-        <h3>CSS Test</h3></span>
+        <span>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>CSS Test</h3>
+        </span>
       </header>
-      <div className="child-container">
-      <ChildComponent title="Component the first"/>
-      <ChildComponent title="Component the second"/>
+      {/* <div className="child-container"> */}
+      <div
+        className={classnames(
+          "flex-column justify-content-center align-content-center"
+        )}
+      >
+        <ChildComponent title="Component the first" />
+        <ChildComponent title="Component the second" />
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ my-app/src/components/funcChild.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import classnames from "classnames";
+
+type FuncChildComponentProps = {
+  title: string;
+};
+
+type FuncChildComponentState = {
+  toggle: boolean;
+};
+
+class FuncChildComponent extends React.Component<
+  FuncChildComponentProps,
+  FuncChildComponentState
+> {
+  state = {
+    toggle: false
+  };
+
+  render() {
+    return (
+      <div
+        className={classnames("border-white flex-column m2 mlr5", {
+          "background-color-purple": this.state.toggle
+        })}
+      >
+        <h2>{this.props.title}</h2>
+        <button className="m2 ml3" onClick={this.onButtonOneClick}>
+          Toggle Style
+        </button>
+        <button className="m2 ml3">Do nothing</button>
+        <input className="m2 ml3" placeholder="Text goes here" />
+      </div>
+    );
+  }
+
+  onButtonOneClick = () => {
+    this.setState({ toggle: !this.state.toggle });
+  };
+}
+
+export default FuncChildComponent;
